c9272ca750e66bc927e114eb8f6528b0f711c7c5
fernandovalencia181 <fernandovalencia181@gmail.com>
2025-12-09 12:29:15 +0100

    Inicio del proyecto eventos

---
+++ database/migrations/2025_12_09_112228_create_eventos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->dateTime('fecha');
+            $table->integer('aforo_maximo'); // Requisito [cite: 42, 58]
+            $table->string('lugar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('eventos');
+    }
+};
